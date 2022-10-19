5482020343d8d0828b9c5a481d555b6a406851f0
sasha-kolosov <77949996+sasha-kolosov@users.noreply.github.com>
2022-10-19 15:40:13 +0300

    Update index.js

--- interests/index.js
+++ interests/index.js
@@ -6,13 +6,25 @@ const twoOne = document.getElementById('2-1')
 const twoTwo = document.getElementById('2-2')
 
 one.addEventListener('change', () => {
-    one.checked = true
-    oneOne.checked = true
-    oneTwo.checked = true
+    if(one.checked == true) {
+        one.checked = true
+        oneOne.checked = true
+        oneTwo.checked = true
+    } else {
+        one.checked = false
+        oneOne.checked = false
+        oneTwo.checked = false
+    }
 })
 
 two.addEventListener('change', () => {
-    two.checked = true
-    twoOne.checked = true
-    twoTwo.checked = true
+    if(two.checked == true) {
+        two.checked = true
+        twoOne.checked = true
+        twoTwo.checked = true
+    } else {
+        two.checked = false
+        twoOne.checked = false
+        twoTwo.checked = false
+    }
 })
